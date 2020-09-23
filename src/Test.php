839f66643eac1d8b763f365948011580aa2f0556
Vaidas <vaidas.stumbras@gmail.com>
2020-09-23 11:09:47 +0300

    kill

--- src/Test.php
+++ src/Test.php
@@ -1,8 +1,20 @@
 <?php
 
-namespace vdxas;
+namespace Vdxas\Test;
 
+/**
+ * A Laravel wrapper for Dompdf
+ *
+ * @package test
+ * @author vdxas
+ */
+ 
 class Test {
+	
+	/**
+     * return string
+	 * @param string $sName
+     */
 	public function vdx(String $sName) {
 		return 'Labas ' . $sName;
 	}
