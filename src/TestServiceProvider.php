654deb829462c2e376865c67772e55429befc0fb
Vaidas <vaidas.stumbras@gmail.com>
2020-09-23 11:10:09 +0300

    killadd

---
+++ src/TestServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Vdxas\Testsss;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routess/webb.php');
+    }
+
